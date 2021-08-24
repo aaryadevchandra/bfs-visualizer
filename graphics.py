fef3dd12993c8822dc555eff1d07188aec4091f6
AaryadevChandra <aaryadevc@gmail.com>
2021-08-24 19:18:32 +0530

    changes

--- graphics.py
+++ graphics.py
@@ -3,6 +3,7 @@ import bfs
 from random import randint
 from time import sleep
 
+
 class Node:
 
     def __init__(self, coords, number):
@@ -10,31 +11,31 @@ class Node:
         self.coords = coords
 
 
-
 def create_node_circle(window, occurred_spots):
 
     node_current_occurrence_coords = (randint(200, 1500), randint(10, 600))
-   
+
     if node_current_occurrence_coords not in occurred_spots:
         occurred_spots.append(node_current_occurrence_coords)
-        pygame.draw.circle(window, (255, 255, 0), node_current_occurrence_coords, 10)
+        pygame.draw.circle(window, (255, 255, 0),
+                           node_current_occurrence_coords, 10)
 
     else:
         create_node_circle(window, occurred_spots)
-        
-    return node_current_occurrence_coords
 
+    return node_current_occurrence_coords
 
 
 def init_graph(graph, node_list, window, occurred_spots):
 
     for i in range(graph.shape[0]):
         node_list.append(Node(create_node_circle(window, occurred_spots), i))
-    
+
     for i in range(graph.shape[0]):
         for j in range(graph.shape[1]):
             if graph[i][j] > 0:
-                pygame.draw.line(window, (255, 255, 255), node_list[i].coords, node_list[j].coords, 2)
+                pygame.draw.line(window, (255, 255, 255),
+                                 node_list[i].coords, node_list[j].coords, 2)
 
 
 def returnNodeIndices(node):
@@ -45,9 +46,11 @@ def returnNodeIndices(node):
 def trackDiscoveredNodes(window, discovered_nodes_render):
 
     for i in range(len(discovered_nodes_render)):
-        pygame.draw.circle(window, (0, 0, 255), discovered_nodes_render[i].coords, 10)
+        pygame.draw.circle(window, (0, 0, 255),
+                           discovered_nodes_render[i].coords, 10)
 
         i += 1
+
 
 def main():
 
@@ -68,18 +71,18 @@ def main():
    #      [0,1,1,0,1,1,1,0,1,0,1,1,1,1,0,0],
    #      [0,1,0,1,0,1,0,1,1,1,1,1,1,0,0,1]])
 
-    graph = bfs.np.array([[0, 0, 1, 1, 0, 0], 
-          [0, 0, 0, 1, 1, 1], 
-          [1, 0, 0, 0, 1, 0], 
-          [1, 1, 0, 0, 0, 0], 
-          [0, 1, 1, 0, 0, 1], 
-          [0, 1, 0, 0, 1, 0]])
+    graph = bfs.np.array([[0, 0, 1, 1, 0, 0],
+                          [0, 0, 0, 1, 1, 1],
+                          [1, 0, 0, 0, 1, 0],
+                          [1, 1, 0, 0, 0, 0],
+                          [0, 1, 1, 0, 0, 1],
+                          [0, 1, 0, 0, 1, 0]])
 
     discovered = {}
     tree = []
 
     tree = bfs.bfs(graph, tree, discovered)
-    
+
     pygame.init()
     window_height = 700
     window_width = 1800
@@ -93,7 +96,6 @@ def main():
 
     init_flag = 0
 
-
     discovered_nodes_render = []
 
     while run:
@@ -101,21 +103,21 @@ def main():
         sleep(2)
 
         if init_flag == 0:
-            init_graph(graph, node_list, window, occurred_spots) 
-            init_flag = 1 
+            init_graph(graph, node_list, window, occurred_spots)
+            init_flag = 1
 
         try:
             node = tree.pop(0)
         except IndexError:
             print("Breadth First Search Complete ")
             run = False
+            exit(0)
 
         i, j = returnNodeIndices(node)
 
         discovered_nodes_render.append(node_list[int(i)])
         discovered_nodes_render.append(node_list[int(j)])
         trackDiscoveredNodes(window, discovered_nodes_render)
-            
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -124,13 +126,11 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     run = False
-         
-        pygame.display.update()
-        
-    pygame.quit()
 
+        pygame.display.update()
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
     main()
-
